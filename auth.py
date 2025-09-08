2696baff629ea1a3bdde54f35f8ec530d74a1e38
Nehemiah <danielnehemiah15@gmail.com>
2025-09-08 22:22:19 +0100

    implement user login with JWT access and refresh tokens

--- auth.py
+++ auth.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
 from models import User
+from flask_jwt_extended import create_access_token, create_refresh_token
 
 auth_bp = Blueprint('auth', __name__)
 
@@ -25,3 +26,23 @@ def status():
     
     return jsonify({"message": "User created successfully"}), 201
         
+@auth_bp.post('/signin')
+def login_user():
+    data = request.get_json()
+    user = User.get_user_by_username(username=data.get('username'))
+    if user and (user.verify_hash(password=data.get('password'))):
+
+        access_token = create_access_token(identity=user.username)
+        refresh_token = create_refresh_token(identity=user.username)
+
+        return jsonify(
+            {
+                'message': 'Logged in successfully',
+                'tokens': {
+                    'access': access_token,
+                    'refresh': refresh_token
+                },
+            }
+            ), 400
+    
+    return jsonify({'message': 'Username or Password is incorrect'}), 200
