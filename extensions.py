a1873ef4d298eba5c284f665bdc95d8fc7dbf522
Nehemiah <danielnehemiah15@gmail.com>
2025-09-08 21:04:48 +0100

    create jwt manager instance

--- extensions.py
+++ extensions.py
@@ -1,4 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
 
 # Create SQLAlchemy instance
 db = SQLAlchemy()
+
+# Create JWT Manager instance
+jwt = JWTManager()
