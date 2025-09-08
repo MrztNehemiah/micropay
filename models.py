7043af1969cda72486c08d9e79a5f5e20071ffd1
Nehemiah <danielnehemiah15@gmail.com>
2025-09-08 01:00:57 +0100

    setup database table and columns

---
+++ models.py
@@ -0,0 +1,18 @@
+from extensions import db
+from uuid import uuid4
+# Import necessary SQLAlchemy types
+from sqlalchemy.dialects.postgresql import UUID
+
+class User(db.Model):
+    # Define the table name and columns
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(UUID(as_uuid=True), default=uuid4, unique=True)  # external identifier
+    firstname = db.Column(db.String(150), nullable=False)
+    lastname = db.Column(db.String(150), nullable=False)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
